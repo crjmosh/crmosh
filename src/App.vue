18cb1eda7d246c39033363180b176944ecd602cc
Cael Jensen <crjmosh@gmail.com>
2020-09-11 16:06:10 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+// import FrontMenu from './components/FrontMenu.vue';
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  data: function() {
+    return {
+
+    }
+  },
+}
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+  }
+  html {
+    font-size: 10px;
+  }
+  body {
+    background-color: #111;
+    margin: 0;
+    padding: 0;
+  }
+  #app {
+    margin: 0 auto;
+    font-family: 'Rubik Mono One', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #fffff3;
+    overflow: hidden;
+  }
+</style>
