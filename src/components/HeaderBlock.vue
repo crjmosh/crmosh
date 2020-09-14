baac445915e5ed7a79a482178ca0cbbc46ded3e9
Cael Jensen <crjmosh@gmail.com>
2020-09-14 16:40:21 -0600

    Add responsive styling to header

--- src/components/HeaderBlock.vue
+++ src/components/HeaderBlock.vue
@@ -36,7 +36,7 @@
     color: #fffff3;
     border-bottom: 15px solid #d00;
     background-image: url('../assets/streak-vertical.svg'), url('../assets/mosh-m.svg'), url('../assets/grid-straight.svg');
-    background-position: calc(50% + 118px) 0, 10px calc(100% - 10px), center;
+    background-position: calc(50% + 108px) 0, 10px calc(100% - 10px), center;
     background-repeat: no-repeat, no-repeat, repeat;
     background-size: auto 80%, 40% auto, 60px;
   }
@@ -45,6 +45,7 @@
     grid-template-columns: max-content auto;
     grid-template-rows: max-content auto;
     margin-top: 4rem;
+    margin-right: 2rem;
     text-transform: uppercase;
   }
   .fmname {
@@ -95,5 +96,77 @@
   .header-menu a:hover {
     color: #d00;
     text-decoration: none;
+  }
+  @media(min-width: 768px) {
+    .wrapper {
+      height: 700px;
+      background-image: url('../assets/streak-diagonal.svg'), url('../assets/mosh-m.svg'), url('../assets/grid-diagonal.svg');
+      background-position: 8.5rem top, calc(100% - 10px) 10px, -16rem 0;
+      background-repeat: no-repeat, no-repeat, no-repeat;
+      background-size: auto 75%, 32% auto, auto 100%;
+      text-align: left;
+    }
+    .text-grid {
+      transform: rotate(-45deg);
+      margin-top: 11rem;
+      margin-left: 5rem;
+      margin-right: 0;
+    }
+    .fname {
+      height: 6rem;
+      font-size: 6rem;
+    }
+    .mname {
+      height: 8rem;
+      font-size: 8rem;
+    }
+    .lname {
+      width: 14rem;
+      font-size: 13rem;
+    }
+    .header-menu {
+      writing-mode: vertical-rl;
+      text-align: left;
+      font-size: 3rem;
+      line-height: 1.4;
+    }
+    .header-menu ul {
+      float: none;
+      padding-top: 2.5rem;
+    }
+  }
+  @media(min-width: 991px) {
+    .wrapper {
+      background-position: 12rem top, calc(100% - 10px) 10px, -12rem 0;
+      background-size: auto 90%, 32% auto, auto 100%;
+    }
+    .text-grid {
+      margin-top: 12rem;
+      margin-left: 5rem;
+    }
+    .fname {
+      height: 8rem;
+      font-size: 8rem;
+    }
+    .mname {
+      height: 10rem;
+      font-size: 10rem;
+    }
+    .lname {
+      width: 17rem;
+      font-size: 16rem;
+    }
+    .header-menu {
+      font-size: 3rem;
+      line-height: 1.4;
+    }
+    .header-menu ul {
+      padding-top: 2.5rem;
+    }
+  }
+  @media(min-width: 1200px) {
+    .wrapper {
+      background-size: auto 90%, auto 60%, auto 100%;
+    }
   }
 </style>
